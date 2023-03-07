e207ba4fe976d55627024c60904fdfe55c536b9a
Daniel Escobar <escobare361@gmail.com>
2023-03-06 23:53:23 -0600

    Add description to Number mirror

--- main1.cpp
+++ main1.cpp
@@ -1,3 +1,21 @@
+/*
+Problem
+Write a program that takes a number N as the input, and prints it to the output.
+
+Input Format
+The only line contains a single integer.
+
+Output Format
+Output the answer in a single line.
+
+Sample 1
+Input: 123, Output: 123
+
+Sample 2
+Input: 14, Output: 14
+ 
+*/
+
 #include <iostream>
 using namespace std;
 
